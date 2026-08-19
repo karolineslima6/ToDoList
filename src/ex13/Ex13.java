78fd810e29fe363e1eda998594e4ceb6b1a53737
karoline <karoline.s.lima6@aluno.senai.br>
2026-08-19 11:00:27 -0300

    alteracoes aula 19/08

--- src/ex13/Ex13.java
+++ src/ex13/Ex13.java
@@ -5,13 +5,93 @@
 package ex13;
 
 import java.awt.SystemColor;
+import java.util.Scanner;
 
 
 public class Ex13 {
 
     public static void main(String[] args) {
-      
-        
-        Scanner scanner = new Scanner ()
-    
+        Scanner scanner = new Scanner(System.in);
+         
+         String[] tarefas = new String [10];
+         boolean[] concluidas = new boolean[10];
+         
+         int opcao = 0;
+         
+         while(opcao != 5){
+             System.out.println("=====lista de tarefas====");
+             System.out.println("1 - adicionar tarefa");
+             System.out.println("2 - listar tarefas");
+             System.out.println("3 - concluir tarefa");
+             System.out.println("4 - excluir tarefa");
+             System.out.println("5 - sair");
+             
+             System.out.println("escolar uma opcao");
+             opcao = scanner.nextInt();
+             scanner.nextLine();
+             
+             switch (opcao) {
+                 case 1:
+                     int posicaoLivre = -1;
+                     
+                     for(int i = 0; i < tarefas.length; i++){
+                         if(tarefas[i] == null){
+                             posicaoLivre = i;
+                             break;
+                         }
+                     }
+                        
+                   if (posicaoLivre == -1) {
+                       System.out.println("a lista de tarefas esta cheia!");
+                    } else{
+                       System.out.println("digite a terefa:");
+                       tarefas[posicaoLivre] = scanner.nextLine();
+                       concluidas[posicaoLivre] = false;
+                       System.out.println("tarefa adicinada com sucesso");
+                   }
+                   break;
+             
+             case 2:
+             System.out.println("===== minhas tarefas======");
+             
+             boolean existeTarefa = false;
+             
+             for(int i = 0; i < tarefas.length; i++) {
+                 if(tarefas[i] != null) {
+                     existeTarefa = true;
+                     
+                     if (concluidas[i] == true){
+                         System.out.println((i + 1) + "- [x] " + tarefas[i]);
+                     }else{
+                         System.out.println((i + 1) + "- [] " + tarefas[i]);
+                     }
+                 }
+             }
+             
+             if (existeTarefa == false){
+                 System.out.println("nenhuma tarefa cadastrada");
+             }
+             
+             break;
+             
+             
+             case 3:
+                System.out.println("Digite o numero da tarefa que deseja concluir:");
+                int numeroConcluir = scanner.nextInt();
+                
+                int indiceConcluir = numeroConcluir - 1;
+                
+                if(indiceConcluir >= 0 &&
+                        indiceConcluir< tarefas.length &&
+                            tarefas [indiceConcluir] != null) {
+                 
+                    concluidas[indiceConcluir] = true;
+                    System.out.println("Tarefa concluida com sucesso!");
+                }else{
+                    System.out.println("Tarefa invalida!");
+                }
+             } 
+         }
+    }
 }
+                       
